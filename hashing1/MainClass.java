0088a67d922ba56ee277e43e214e3472d319d865
Himanshu Kumar <himanshuishwar399@gmail.com>
2023-12-29 13:17:55 +0530

    HashMap and its working

--- hashing1/MainClass.java
+++ hashing1/MainClass.java
@@ -1,2 +1,48 @@
-package hashing1;public class MainClass {
+package hashing1;
+import java.util.*;
+import java.util.Objects;
+
+public class MainClass {
+    public static void main(String[] args) {
+        String s ="abc";
+
+        System.out.println(s.hashCode());
+
+//        Boolean bol =true;
+//        System.out.println(bol.hashCode());
+
+//        Integer a =12;
+//        System.out.println(a.hashCode());
+
+Boy b1 =new Boy(25,"Anuj");
+Boy b2 =b1;
+Boy b3 =new Boy(23, "Anuj");
+        System.out.println(b1.hashCode());
+        System.out.println(b2.hashCode());
+        System.out.println(b3.hashCode());
+
+        HashMap<Integer, String> map =new HashMap<>();
+        map.put(12, "Himanshu");
+    }
+
+
+static class Boy{
+        int age;
+        String name;
+
+      public Boy(int age, String name){
+          this.age =age;
+          this.name =name;
+      }
+
+
+      @Override
+     public int hashCode(){
+        return Objects.hash(name, age);
+}
+
+
+
+}
+
 }
--- hashing2/MainClass.java
+++ hashing2/MainClass.java
@@ -1,2 +1,67 @@
-package hashing2;public class MainClass {
+package hashing2;
+import java.util.*;
+public class MainClass {
+    public static void main(String[] args) {
+          int []a ={10,20,20,10,30,10};
+        //System.out.println(countDistinctElement(a));
+       // printFreqOfElement(a);
+       // System.out.println(isTwoSum(a,70));
+        int []ans =twoSumUsingMap(a,40);
+        System.out.println(ans[0]+" "+ ans[1]);
+    }
+
+    static int[] twoSumUsingMap(int []a, int value){
+        Map<Integer,Integer> map =new HashMap<>();
+        int ans[] ={-1,-1};
+        for(int i=0; i<a.length; i++){
+            if(map.containsKey(value-a[i])){
+               a[0] =  map.get(value-a[i]);
+               a[i] =i;
+               return ans;
+            }else {
+                map.putIfAbsent(a[i],i);
+            }
+
+        }
+        return ans;
+    }
+
+    static boolean isTwoSum(int []a, int value){
+        HashSet<Integer> set =new  HashSet<>();
+        for(int e: a){
+            if(set.contains(value-e)){
+                return true;
+            }
+            set.add(e);
+
+        }
+        return false;
+    }
+
+
+    static void printFreqOfElement(int a[]){
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int e: a){
+            if(map.containsKey(e)){
+                map.put(e, map.get(e)+1);
+            }else {
+                map.put(e,1);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> entries: map.entrySet()){
+            System.out.print(entries.getKey()+" -> "+ entries.getValue());
+            System.out.println();
+        }
+
+    }
+    static int countDistinctElement(int []a){
+        HashSet<Integer> set  =new HashSet<>();
+        for(int e: a){
+            set.add(e);
+        }
+        return set.size();
+    }
 }
