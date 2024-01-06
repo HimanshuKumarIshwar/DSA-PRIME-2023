acb5ed5ff5797e466d30fc2e424ae6b5c2ef8ea9
Himanshu Kumar <himanshuishwar399@gmail.com>
2024-01-06 08:22:39 +0530

    Graph Dfs and releted question

--- graph1/MainClass.java
+++ graph1/MainClass.java
@@ -26,21 +26,37 @@ public class MainClass {
 //       addEdgeMat(graphMat,1 , 2, 5);
 //       addEdgeMat(graphMat, 3, 2, 3);
 
-   boolean[] visit =new boolean[v];
-   int dist[]= new int[v];
-   int src =0;
+//   boolean[] visit =new boolean[v];
+//   int dist[]= new int[v];
+//   int prec[] = new int[v];
+//   int src =0;
 
-   bfs(graph,src,visit,dist);
-   for(int i=0; i<v; i++){
-       System.out.println(i+ " ->" + dist[i]);
+//   bfs(graph,src,visit,dist, prec);
+//   for(int i=0; i<v; i++){
+//       System.out.println(i+ " ->" + dist[i]);
+//   }
+//
    }
+    static void findPath(ArrayList<ArrayList<Integer>> graph, int src, int dest){
+             int v =4;
+            boolean[] visit =new boolean[v];
+            int dist[]= new int[v];
+            int prec[] = new int[v];
+
+            bfs(graph,src,visit,dist, prec);
+     while(dest !=-1){
+         System.out.println(dest);
+          dest =prec[dest];
+     }
+
 
     }
-    static void bfs(ArrayList<ArrayList<Integer>> graph, int src, boolean []visit, int[] dist){
+    static void bfs(ArrayList<ArrayList<Integer>> graph, int src, boolean []visit, int[] dist,int[] prec){
           Queue<Integer> q =new LinkedList<>();
 
           q.add(src);
           visit[src] = true;
+          prec[src] =-1;
         dist[src] =0;
           while(!q.isEmpty()){
                int cur =q.poll();
@@ -50,6 +66,7 @@ public class MainClass {
                       q.add(neighbours);
                       visit[neighbours] =true;
                       dist[neighbours] =dist[cur]+1;
+                      prec[neighbours] =cur;
                   }
               }
           }
